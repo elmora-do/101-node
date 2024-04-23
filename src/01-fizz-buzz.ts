67d7db6d82a68caf1017b8ec053d0c0b7803ce05
Luis Morales <luis.morales.d1@gmail.com>
2024-04-23 13:57:24 -0600

    fizz buzz

--- src/01-fizz-buzz.ts
+++ src/01-fizz-buzz.ts
@@ -1,1 +1,28 @@
-console.log('fizz buzz');
+/*
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
+console.log('********** 01 FizzBuzz **********');
+
+const from = 1;
+const to = 100;
+const fizz = 'fizz';
+const buzz = 'buzz';
+
+for (let index = from; index <= to; index++) {
+  if (index % 3 === 0 && index % 5 === 0) {
+    console.log(`${fizz}${buzz}\n`);
+  } else if (index % 3 === 0) {
+    console.log(`${fizz}\n`);
+  } else if (index % 5 === 0) {
+    console.log(`${buzz}\n`);
+  } else {
+    console.log(`${index}\n`);
+  }
+}
+
+console.log('********** 01 FizzBuzz FIN **********');
